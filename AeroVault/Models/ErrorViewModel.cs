ca63f0052d1b2fa4a6209946726c06205f6f2f1f
pramodh-srilankan <in1957@srilankan.com>
2024-11-04 11:49:45 +0530

    Add project files.

---
+++ AeroVault/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AeroVault.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
