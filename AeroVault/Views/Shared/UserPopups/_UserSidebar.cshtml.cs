b97203ac4c8f558704256ddd80acf3bd6c58e163
Pramodh Lihinikaduwa <pramodh123kit@gmail.com>
2024-11-18 14:44:30 +0530

    somthing

---
+++ AeroVault/Views/Shared/UserPopups/_UserSidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AeroVault.Views.Shared.UserPopups
+{
+    public class _UserSidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
