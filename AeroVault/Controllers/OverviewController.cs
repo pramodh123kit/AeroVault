abbe33ab6451c15bfa8e246762184e73cbf79bc1
Chamath madhusanka <141642385+ChamathMadhu@users.noreply.github.com>
2025-01-06 09:45:06 +0530

    update BE

--- AeroVault/Controllers/OverviewController.cs
+++ AeroVault/Controllers/OverviewController.cs
@@ -1,22 +1,23 @@
 ﻿using AeroVault.Controllers;
 using AeroVault.Data; // Include your repositories
 using AeroVault.Models; // Include your models
+using AeroVault.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 public class OverviewController : BaseAdminController
 {
-    private readonly DivisionRepository _divisionRepository;
+    private readonly DepartmentRepository _departmentRepository;
     private readonly SystemRepository _systemRepository;
 
     public async Task<IActionResult> Index()
     {
-        var divisions = await _divisionRepository.GetAllDivisionsAsync();
+        var departments = await _departmentRepository.GetAllDepartmentsAsync();
         var systems = await _systemRepository.GetAllSystemsAsync();
 
         var viewModel = new OverviewViewModel
         {
-            DepartmentCount = divisions.Count,
+            DepartmentCount = departments.Count,
             SystemCount = systems.Count,
             DocumentCount = 0, // Set to 0 or fetch from the database if needed
             VideoCount = 0 // Set to 0 or fetch from the database if needed
@@ -29,10 +30,10 @@ public class OverviewController : BaseAdminController
         return PartialView("~/Views/Admin/_Overview.cshtml", viewModel);
     }
 
-    public OverviewController(ApplicationDbContext context, DivisionRepository divisionRepository, SystemRepository systemRepository)
+    public OverviewController(ApplicationDbContext context, DepartmentRepository departmentRepository, SystemRepository systemRepository)
         : base(context)
     {
-        _divisionRepository = divisionRepository;
+        _departmentRepository = departmentRepository;
         _systemRepository = systemRepository;
     }
 }
