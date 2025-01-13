4363ff54bc73c26695adb1239b751a674ebddaca
Pramodh Lihinikaduwa <pramodh123kit@gmail.com>
2025-01-13 19:00:10 +0530

    fixed upload popup

--- AeroVault/Scripts/AdminUpload.js
+++ AeroVault/Scripts/AdminUpload.js
@@ -440,3 +440,23 @@ document.addEventListener('click', function (event) {
         tooltip.style.display = 'none';
     }
 });
+
+
+function fileEditopenPopup8() {
+    document.getElementById("dark-overlay8").style.display = "block";
+    document.getElementById("editfile-popup8").style.display = "block";
+
+
+    goToStep(1);
+}
+
+function fileEditClosePopup8() {
+    document.getElementById("dark-overlay8").style.display = "none";
+    document.getElementById("editfile-popup8").style.display = "none";
+}
+
+document.querySelectorAll(".upload-btn").forEach(function (icon) {
+    icon.onclick = fileEditopenPopup8;
+});
+
+document.getElementById("close-logout8").onclick = fileEditClosePopup8;
