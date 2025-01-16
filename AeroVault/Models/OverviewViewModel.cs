a9c79739cd7ce45dba62ebd4832513d71e741590
Chamath madhusanka <141642385+ChamathMadhu@users.noreply.github.com>
2025-01-16 15:40:02 +0530

    sick

--- AeroVault/Models/OverviewViewModel.cs
+++ AeroVault/Models/OverviewViewModel.cs
@@ -7,14 +7,16 @@ namespace AeroVault.Models
         public int DepartmentCount { get; set; }
         public int SystemCount { get; set; }
         public int DocumentCount { get; set; }
-        public int VideoCount { get; set; }
-        public int DivisionCount { get; set; }
 
+
+        public int DivisionCount { get; set; }
         public int Document_1_Count { get; set; }
         public int Document_3_Count { get; set; }
         public int Document_6_Count { get; set; }
         public int Document_12_Count { get; set; }
 
+
+        public int VideoCount { get; set; }
         public int Video_1_Count { get; set; }
         public int Video_3_Count { get; set; }
         public int Video_6_Count { get; set; }
