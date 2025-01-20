042b4555aaaa6d172adf176116c2f94a83062403
Chamath madhusanka <141642385+ChamathMadhu@users.noreply.github.com>
2025-01-20 16:25:35 +0530

    merging so that you know i merged something but if u really wanna know, the chartjs now loads when you ( ofc) go to a different tab and come back xD

--- AeroVault/Scripts/AdminOverview.js
+++ AeroVault/Scripts/AdminOverview.js
@@ -1,155 +1,153 @@
 ﻿let myChart; // Declare myChart in the global scope
 let myChart2; // Declare myChart2 in the global scope
 
-(function () {
+function initializeCharts() {
     const docs = document.querySelector('.stats-container .stat-item[data-all-time-docs]');
     const videos = document.querySelector('.stats-container .stat-item[data-all-time-videos]');
     const systems = document.querySelector('.stats-container .stat-item[data-all-time-systems]');
     const departments = document.querySelector('.stats-container .stat-item[data-all-time-departments]');
     const divisions = document.querySelector('.stats-container .stat-item[data-all-time-divisions]');
 
-    function initializeCharts() {
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const ctx2 = document.getElementById('myChart2').getContext('2d');
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const ctx2 = document.getElementById('myChart2').getContext('2d');
 
-        // Create gradients
-        const gradient1 = ctx.createLinearGradient(0, 0, 400, 400);
-        gradient1.addColorStop(0, '#36A2EB'); // Start color blue
-        gradient1.addColorStop(1, '#05448B'); // End color
+    // Create gradients
+    const gradient1 = ctx.createLinearGradient(0, 0, 400, 400);
+    gradient1.addColorStop(0, '#36A2EB'); // Start color blue
+    gradient1.addColorStop(1, '#05448B'); // End color
 
-        const gradient2 = ctx.createLinearGradient(0, 0, 800, 400);
-        gradient2.addColorStop(0, '#C9AA22'); // Start color yellow
-        gradient2.addColorStop(1, '#635411'); // End color
+    const gradient2 = ctx.createLinearGradient(0, 0, 800, 400);
+    gradient2.addColorStop(0, '#C9AA22'); // Start color yellow
+    gradient2.addColorStop(1, '#635411'); // End color
 
-        const gradient3 = ctx.createLinearGradient(0, 0, 800, 400);
-        gradient3.addColorStop(0, '#CE1D1D'); // Start color red
-        gradient3.addColorStop(1, '#680F0F'); // End color
+    const gradient3 = ctx.createLinearGradient(0, 0, 800, 400);
+    gradient3.addColorStop(0, '#CE1D1D'); // Start color red
+    gradient3.addColorStop(1, '#680F0F'); // End color
 
-        const gradient4 = ctx.createLinearGradient(0, 0, 800, 400);
-        gradient4.addColorStop(0, '#2E8D58'); // Start color Green
-        gradient4.addColorStop(1, '#04612D'); // End color
+    const gradient4 = ctx.createLinearGradient(0, 0, 800, 400);
+    gradient4.addColorStop(0, '#2E8D58'); // Start color Green
+    gradient4.addColorStop(1, '#04612D'); // End color
 
-        const gradient5 = ctx.createLinearGradient(0, 0, 800, 400);
-        gradient5.addColorStop(0, '#FF6333'); // Start color orange
-        gradient5.addColorStop(1, '#993B1F'); // End color
+    const gradient5 = ctx.createLinearGradient(0, 0, 800, 400);
+    gradient5.addColorStop(0, '#FF6333'); // Start color orange
+    gradient5.addColorStop(1, '#993B1F'); // End color
 
-        const gradient6 = ctx.createLinearGradient(0, 0, 800, 400);
-        gradient6.addColorStop(0, '#053C7A'); // Start color orange
-        gradient6.addColorStop(1, '#052850'); // End color
+    const gradient6 = ctx.createLinearGradient(0, 0, 800, 400);
+    gradient6.addColorStop(0, '#053C7A'); // Start color orange
+    gradient6.addColorStop(1, '#052850'); // End color
 
-        const gradient7 = ctx2.createLinearGradient(0, 0, 800, 0);
-        gradient7.addColorStop(0, 'rgba(5, 60, 122, 0.90)');
-        gradient7.addColorStop(1, 'rgba(5, 40, 80, 0.90)');
+    const gradient7 = ctx2.createLinearGradient(0, 0, 800, 0);
+    gradient7.addColorStop(0, 'rgba(5, 60, 122, 0.90)');
+    gradient7.addColorStop(1, 'rgba(5, 40, 80, 0.90)');
 
-        let selectedDocs = docs ? docs.getAttribute('data-all-time-docs') : null;
-        let selectedVideos = videos ? videos.getAttribute('data-all-time-videos') : null;
-        let selectedSystems = systems ? systems.getAttribute('data-all-time-systems') : null;
-        let selectedDepartments = departments ? departments.getAttribute('data-all-time-departments') : null;
-        let selectedDivisions = divisions ? divisions.getAttribute('data-all-time-divisions') : null;
+    let selectedDocs = docs ? docs.getAttribute('data-all-time-docs') : null;
+    let selectedVideos = videos ? videos.getAttribute('data-all-time-videos') : null;
+    let selectedSystems = systems ? systems.getAttribute('data-all-time-systems') : null;
+    let selectedDepartments = departments ? departments.getAttribute('data-all-time-departments') : null;
+    let selectedDivisions = divisions ? divisions.getAttribute('data-all-time-divisions') : null;
 
-        const dataset = [{
-            label: 'Total ',
-            data: [selectedSystems, selectedDepartments, selectedDocs, selectedVideos, selectedDivisions],
+    const dataset = [{
+        label: 'Total ',
+        data: [selectedSystems, selectedDepartments, selectedDocs, selectedVideos, selectedDivisions],
+        borderWidth: 0,
+        backgroundColor: [gradient3, gradient4, gradient5, gradient2, gradient1], // Segment colors
+        hoverBackgroundColor: ['#CE1D1D', '#2E8D58', '#FF6333', '#C9AA22', '#36A2EB'] // Hover colors
+    }];
+
+    function calculateBarThickness() {
+        const screenWidth = window.innerWidth;
+        return Math.max(30, Math.min(70, screenWidth / 15)); // Adjust the divisor as needed
+    }
+
+    function renderCharts() {
+        const barThickness = calculateBarThickness();
+
+        const dataset2 = [{
+            label: 'Logged In Staff  ',
+            data: [120, 190, 100, 200, 100],
             borderWidth: 0,
-            backgroundColor: [gradient3, gradient4, gradient5, gradient2, gradient1], // Segment colors
-            hoverBackgroundColor: ['#CE1D1D', '#2E8D58', '#FF6333', '#C9AA22', '#36A2EB'] // Hover colors
+            backgroundColor: [gradient7], // Segment colors
+            hoverBackgroundColor: ['#36A2EB'], // Hover colors
+            barThickness: barThickness,
+            borderRadius: {
+                topLeft: 10,
+                topRight: 10,
+                bottomLeft: 0,
+                bottomRight: 0
+            }
         }];
 
-        function calculateBarThickness() {
-            const screenWidth = window.innerWidth;
-            return Math.max(30, Math.min(70, screenWidth / 15)); // Adjust the divisor as needed
+        // Clear existing charts
+        if (myChart) {
+            myChart.destroy(); // Call destroy on the existing doughnut chart instance
+        }
+        if (myChart2) {
+            myChart2.destroy(); // Call destroy on the existing bar chart instance
         }
 
-        function renderCharts() {
-            const barThickness = calculateBarThickness();
-
-            const dataset2 = [{
-                label: 'Logged In Staff  ',
-                data: [120, 190, 100, 200, 100],
-                borderWidth: 0,
-                backgroundColor: [gradient7], // Segment colors
-                hoverBackgroundColor: ['#36A2EB'], // Hover colors
-                barThickness: barThickness,
-                borderRadius: {
-                    topLeft: 10,
-                    topRight: 10,
-                    bottomLeft: 0,
-                    bottomRight: 0
-                }
-            }];
-
-            // Clear existing charts
-            if (myChart) {
-                myChart.destroy(); // Call destroy on the existing doughnut chart instance
-            }
-            if (myChart2) {
-                myChart2.destroy(); // Call destroy on the existing bar chart instance
-            }
-
-            // Create doughnut chart
-            myChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: ["Systems", "Departments", "Documents", "Videos", "Divisions"],
-                    datasets: dataset
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    plugins: {
-                        legend: {
-                            display: false
+        // Create doughnut chart
+        myChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ["Systems", "Departments", "Documents", "Videos", "Divisions"],
+                datasets: dataset
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        bodyFont: {
+                            size: 14 // Change this to your desired font size
                         },
-                        tooltip: {
-                            bodyFont: {
-                                size: 14 // Change this to your desired font size
-                            },
-                            titleFont: {
-                                size: 15 // Change this to your desired title font size
-                            }
+                        titleFont: {
+                            size: 15 // Change this to your desired title font size
                         }
                     }
                 }
-            });
+            }
+        });
 
-            // Create bar chart
-            myChart2 = new Chart(ctx2, {
-                type: 'bar',
-                data: {
-                    labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
-                    datasets: dataset2
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    plugins: {
-                        legend: {
-                            display: false
+        // Create bar chart
+        myChart2 = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+                datasets: dataset2
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        bodyFont: {
+                            size: 14 // Change this to your desired font size
                         },
-                        tooltip: {
-                            bodyFont: {
-                                size: 14 // Change this to your desired font size
-                            },
-                            titleFont: {
-                                size: 15 // Change this to your desired title font size
-                            }
+                        titleFont: {
+                            size: 15 // Change this to your desired title font size
                         }
                     }
                 }
-            });
-        }
-
-        renderCharts(); // Initial render
-
-        // Add event listener for window resize
-        window.addEventListener('resize', renderCharts);
+            }
+        });
     }
 
-    // Call initializeCharts only if the charts are not already initialized
-    if (!myChart && !myChart2) {
-        setTimeout(initializeCharts, 900);
-    }
-})();
+    renderCharts(); // Initial render
+
+    // Add event listener for window resize
+    window.addEventListener('resize', renderCharts);
+}
+
+// Call initializeCharts only if the charts are not already initialized
+if (!myChart && !myChart2) {
+    setTimeout(initializeCharts, 900);
+}
 
 function toggleCustomDropdown() {
     const dropdownContent = document.querySelector('.custom-dropdown-content');
