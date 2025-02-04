6d02aeec8a28e8c58284dd6cd432c437ec78cc83
Pramodh Lihinikaduwa <pramodh123kit@gmail.com>
2025-02-04 13:45:57 +0530

    FileRepository CSS updated a bit

--- AeroVault/Program.cs
+++ AeroVault/Program.cs
@@ -112,7 +112,8 @@ namespace AeroVault
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Admin}/{action=Index}/{id?}");
+                //pattern: "{controller=Admin}/{action=Index}/{id?}");
+                pattern: "{controller=userfilerepository}/{action=filerepository}/{id?}");
             //pattern: "{controller=test}/{action=testconnection}/{id?}");
 
             app.Run();
