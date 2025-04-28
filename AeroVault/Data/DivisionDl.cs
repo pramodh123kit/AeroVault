cb43ad18093327106dc668bf7418a9e0eb624864
Lahiru Madushanka <23869@srilankan.com>
2025-04-28 07:41:44 +0530

    Division Changes front end debug stop

--- AeroVault/Data/DivisionDl.cs
+++ AeroVault/Data/DivisionDl.cs
@@ -4,7 +4,7 @@ using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration; 
+using Microsoft.Extensions.Configuration;
 
 namespace AeroVault.Data
 {
@@ -55,6 +55,9 @@ namespace AeroVault.Data
             return divisions;
         }
 
+
+        // calling methord is totally wrong needs to calling from Business layer (Overview)
+
         public async Task<List<DivisionModel>> GetAllDivisionsAsync()
         {
             using (var connection = new OracleConnection(_connectionString))
@@ -87,22 +90,178 @@ namespace AeroVault.Data
             }
         }
 
-        public async Task AddDivisionAsync(string divisionName)
+
+        //public async Task<List<DepartmentModel>> GetDepartmentsByDivisionAsync(int divisionId)
+        //{
+        //    using (var connection = new OracleConnection(_connectionString))
+        //    {
+        //        await connection.OpenAsync();
+        //        string sql = "SELECT * FROM DEPARTMENTS WHERE DivisionID = :DivisionID AND IS_DELETED = 0";
+        //        using (var command = new OracleCommand(sql, connection))
+        //        {
+        //            command.Parameters.Add(new OracleParameter(":DivisionID", divisionId));
+        //            using (var reader = await command.ExecuteReaderAsync())
+        //            {
+        //                var departments = new List<DepartmentModel>();
+        //                while (await reader.ReadAsync())
+        //                {
+        //                    departments.Add(new DepartmentModel
+        //                    {
+        //                        DepartmentID = reader.GetInt32(0),
+        //                        DepartmentName = reader.GetString(1),
+        //                        DivisionID = reader.GetInt32(2),
+        //                        IsDeleted = reader.GetInt32(3)
+        //                    });
+        //                }
+        //                return departments;
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        //public async Task AddDivisionAsync(string divisionName)
+        //{
+        //    if (string.IsNullOrEmpty(divisionName))
+        //    {
+        //        throw new ArgumentException("Division name cannot be empty.");
+        //    }
+
+        //    using (var connection = new OracleConnection(_connectionString))
+        //    {
+        //        await connection.OpenAsync();
+
+        //        int newDivisionId;
+        //        string getIdSql = "SELECT SEQ_DIVISIONID.NEXTVAL FROM DUAL";
+        //        using (var idCommand = new OracleCommand(getIdSql, connection))
+        //        {
+        //            newDivisionId = Convert.ToInt32(await idCommand.ExecuteScalarAsync());
+        //        }
+
+        //        string sql = "INSERT INTO DIVISIONS (DivisionID, DivisionName) VALUES (:DivisionID, :DivisionName)";
+        //        using (var command = new OracleCommand(sql, connection))
+        //        {
+        //            command.Parameters.Add(new OracleParameter(":DivisionID", newDivisionId));
+        //            command.Parameters.Add(new OracleParameter(":DivisionName", divisionName));
+
+        //            int rowsAffected = await command.ExecuteNonQueryAsync();
+
+        //            if (rowsAffected == 0)
+        //            {
+        //                throw new Exception("No rows were inserted.");
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public async Task UpdateDivisionNameAsync(string originalName, string newDivisionName)
+        //{
+        //    string sql = @"
+        //    UPDATE DIVISIONS 
+        //    SET DivisionName = :NewDivisionName 
+        //    WHERE DivisionName = :OriginalName";
+
+        //    var parameters = new[]
+        //    {
+        //        new OracleParameter(":NewDivisionName", newDivisionName),
+        //        new OracleParameter(":OriginalName", originalName)
+        //    };
+
+        //    int rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+
+        //    if (rowsAffected == 0)
+        //    {
+        //        throw new Exception("No rows were updated. Division may not exist.");
+        //    }
+        //}
+
+        //public async Task<bool> SoftDeleteDivisionAsync(int divisionId)
+        //{
+        //    using (var connection = new OracleConnection(_connectionString))
+        //    {
+        //        await connection.OpenAsync();
+
+        //        using (var transaction = connection.BeginTransaction())
+        //        {
+        //            try
+        //            {
+        //                string deleteDepartmentsSql = @"
+        //            UPDATE DEPARTMENTS 
+        //            SET IS_DELETED = 1 
+        //            WHERE DIVISIONID = :DivisionId";
+
+        //                using (var departmentsCommand = new OracleCommand(deleteDepartmentsSql, connection))
+        //                {
+        //                    departmentsCommand.Transaction = transaction;
+        //                    departmentsCommand.Parameters.Add(new OracleParameter(":DivisionId", divisionId));
+        //                    await departmentsCommand.ExecuteNonQueryAsync();
+        //                }
+
+        //                string updateDivisionSql = @"
+        //            UPDATE DIVISIONS 
+        //            SET IsDeleted = 1 
+        //            WHERE DivisionID = :DivisionId";
+
+        //                using (var divisionCommand = new OracleCommand(updateDivisionSql, connection))
+        //                {
+        //                    divisionCommand.Transaction = transaction;
+        //                    divisionCommand.Parameters.Add(new OracleParameter(":DivisionId", divisionId));
+        //                    int rowsAffected = await divisionCommand.ExecuteNonQueryAsync();
+
+        //                    transaction.Commit();
+
+        //                    return rowsAffected > 0;
+        //                }
+        //            }
+        //            catch (Exception)
+        //            {
+        //                transaction.Rollback();
+        //                throw;
+        //            }
+        //        }
+        //    }
+        //}
+
+
+
+        public bool updateDivisionByID(int DivisionID, string DivisionName)
         {
-            if (string.IsNullOrEmpty(divisionName))
+            string sql = @"
+            UPDATE DIVISIONS 
+            SET DIVISIONNAME = :DivisionName 
+            WHERE DIVISIONID = :DivisionID";
+
+            var parameters = new[]
             {
-                throw new ArgumentException("Division name cannot be empty.");
+                new OracleParameter(":DivisionName", DivisionName),
+                   new OracleParameter(":DivisionID", DivisionID)
+
+            };
+
+            int rowsAffected = _context.Database.ExecuteSqlRaw(sql, parameters);
+
+            if (rowsAffected == 0)
+            {
+                throw new Exception("No rows were updated. Division may not exist.");
             }
+            return true;
+        }
+
+
+
+        public bool AddDivision(string divisionName)
+        {
 
             using (var connection = new OracleConnection(_connectionString))
             {
-                await connection.OpenAsync();
+                connection.Open();
 
                 int newDivisionId;
                 string getIdSql = "SELECT SEQ_DIVISIONID.NEXTVAL FROM DUAL";
+
                 using (var idCommand = new OracleCommand(getIdSql, connection))
                 {
-                    newDivisionId = Convert.ToInt32(await idCommand.ExecuteScalarAsync());
+                    newDivisionId = Convert.ToInt32(idCommand.ExecuteScalar());
                 }
 
                 string sql = "INSERT INTO DIVISIONS (DivisionID, DivisionName) VALUES (:DivisionID, :DivisionName)";
@@ -111,7 +270,7 @@ namespace AeroVault.Data
                     command.Parameters.Add(new OracleParameter(":DivisionID", newDivisionId));
                     command.Parameters.Add(new OracleParameter(":DivisionName", divisionName));
 
-                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    int rowsAffected = command.ExecuteNonQuery();
 
                     if (rowsAffected == 0)
                     {
@@ -119,100 +278,96 @@ namespace AeroVault.Data
                     }
                 }
             }
+            return true;
         }
 
-        public async Task UpdateDivisionNameAsync(string originalName, string newDivisionName)
+
+        public bool enableDivision(int DivisionID)
         {
             string sql = @"
-            UPDATE DIVISIONS 
-            SET DivisionName = :NewDivisionName 
-            WHERE DivisionName = :OriginalName";
+BEGIN
+    UPDATE DEPARTMENTS 
+    SET IS_DELETED = 0 
+    WHERE DIVISIONID = :DivisionId;
+
+    UPDATE DIVISIONS 
+    SET ISDELETED = 0 
+    WHERE DIVISIONID = :DivisionId;
+END;";
 
             var parameters = new[]
             {
-                new OracleParameter(":NewDivisionName", newDivisionName),
-                new OracleParameter(":OriginalName", originalName)
+
+                  new OracleParameter(":DivisionID", DivisionID)
+
             };
 
-            int rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+            int rowsAffected = _context.Database.ExecuteSqlRaw(sql, parameters);
 
             if (rowsAffected == 0)
             {
                 throw new Exception("No rows were updated. Division may not exist.");
             }
+            return true;
         }
 
-        public async Task<bool> SoftDeleteDivisionAsync(int divisionId)
+
+        public bool disableDivision(int DivisionID)
         {
-            using (var connection = new OracleConnection(_connectionString))
+            string sql = @"
+BEGIN
+    UPDATE DEPARTMENTS 
+    SET IS_DELETED = 1 
+    WHERE DIVISIONID = :DivisionId;
+
+    UPDATE DIVISIONS 
+    SET ISDELETED = 1 
+    WHERE DIVISIONID = :DivisionId;
+END;";
+
+            var parameters = new[]
             {
-                await connection.OpenAsync();
 
-                using (var transaction = connection.BeginTransaction())
-                {
-                    try
-                    {
-                        string deleteDepartmentsSql = @"
-                    UPDATE DEPARTMENTS 
-                    SET IS_DELETED = 1 
-                    WHERE DIVISIONID = :DivisionId";
+                  new OracleParameter(":DivisionID", DivisionID)
 
-                        using (var departmentsCommand = new OracleCommand(deleteDepartmentsSql, connection))
-                        {
-                            departmentsCommand.Transaction = transaction;
-                            departmentsCommand.Parameters.Add(new OracleParameter(":DivisionId", divisionId));
-                            await departmentsCommand.ExecuteNonQueryAsync();
-                        }
+            };
 
-                        string updateDivisionSql = @"
-                    UPDATE DIVISIONS 
-                    SET IsDeleted = 1 
-                    WHERE DivisionID = :DivisionId";
+            int rowsAffected = _context.Database.ExecuteSqlRaw(sql, parameters);
 
-                        using (var divisionCommand = new OracleCommand(updateDivisionSql, connection))
-                        {
-                            divisionCommand.Transaction = transaction;
-                            divisionCommand.Parameters.Add(new OracleParameter(":DivisionId", divisionId));
-                            int rowsAffected = await divisionCommand.ExecuteNonQueryAsync();
-
-                            transaction.Commit();
-
-                            return rowsAffected > 0;
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        transaction.Rollback();
-                        throw;
-                    }
-                }
+            if (rowsAffected == 0)
+            {
+                throw new Exception("No rows were updated. Division may not exist.");
             }
+            return true;
         }
 
-        public async Task<List<DepartmentModel>> GetDepartmentsByDivisionAsync(int divisionId)
+        public List<DivisionModel> GetAllDivisions()
         {
             using (var connection = new OracleConnection(_connectionString))
             {
-                await connection.OpenAsync();
-                string sql = "SELECT * FROM DEPARTMENTS WHERE DivisionID = :DivisionID AND IS_DELETED = 0";
-                using (var command = new OracleCommand(sql, connection))
+                connection.OpenAsync();
+                using (var command = new OracleCommand(
+                    "SELECT DivisionID, DivisionName, IsDeleted, ADDED_DATE " +
+                    "FROM DIVISIONS ",
+                    connection))
                 {
-                    command.Parameters.Add(new OracleParameter(":DivisionID", divisionId));
-                    using (var reader = await command.ExecuteReaderAsync())
+                    var divisions = new List<DivisionModel>();
+                    using (var reader = command.ExecuteReader())
                     {
-                        var departments = new List<DepartmentModel>();
-                        while (await reader.ReadAsync())
+                        while (reader.Read())
                         {
-                            departments.Add(new DepartmentModel
+                            divisions.Add(new DivisionModel
                             {
-                                DepartmentID = reader.GetInt32(0),
-                                DepartmentName = reader.GetString(1),
-                                DivisionID = reader.GetInt32(2),
-                                IsDeleted = reader.GetInt32(3)
+                                DivisionID = Convert.ToInt32(reader["DivisionID"]),
+                                DivisionName = reader["DivisionName"].ToString(),
+                                IsDeleted = Convert.ToInt32(reader["IsDeleted"]),
+                                AddedDate = reader["ADDED_DATE"] != DBNull.Value
+                                    ? Convert.ToDateTime(reader["ADDED_DATE"])
+                                    : (DateTime?)null
                             });
                         }
-                        return departments;
                     }
+                    return divisions;
                 }
             }
         }
